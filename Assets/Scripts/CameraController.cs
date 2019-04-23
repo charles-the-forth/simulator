f754c9555d5409fba40c2f9ae5b8a0b10fac8a17
Václav Pavlíček <vendasky@email.cz>
2019-04-23 20:21:02 +0200

    Add camera rotaion after mouse cursor.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float speedHorizontal = 2.0f;
+    public float speedVertical = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedHorizontal * Input.GetAxis("Mouse X");
+        pitch -= speedVertical * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
